3652a20147622e181c0c5b8f0cc0eb79accaec38
seanmather <smtothemax1@gmail.com>
2017-05-18 10:36:26 -0500

    Update Monopoly.java

--- Monopoly.java
+++ Monopoly.java
@@ -9,6 +9,7 @@ package monopoly;
  *
  * @author 761843
  */
+import java.util.Scanner;
 public class Monopoly {
 
     /**
@@ -16,6 +17,70 @@ public class Monopoly {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Die d1 = new Die();
+        Die d2 = new Die();
+        
+        Scanner scan = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many players? (2-4)");
+        int playernum= in.nextInt();
+        
+            System.out.println("Player "+(1)+", What is your name?");
+            String name = scan.next();
+        
+            Player p1 = new Player(name, 1, "Dog");
+            System.out.println("Player "+(2)+", What is your name?");
+            name = scan.next();
+            Player p2 = new Player(name, 2, "Top Hat");
+  
+          /* if(playernum==3){
+           System.out.println("Player "+(3)+", What is your name?");
+            name = scan.next();
+               Player p3 = new Player(name, 3, "Money Bags");
+           }
+
+            if(playernum==4){
+           System.out.println("Player "+(3)+", What is your name?");
+            name = scan.next();
+               Player p3 = new Player(name, 3, "Money Bags");
+               System.out.println("Player "+(3)+", What is your name?");
+            name = scan.next();
+           Player p4 = new Player(name, 4, "Race Car");
+           }        */
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+       boolean play= true;
+       int turn = 1;
+       boolean rollpermission=true;
+       int roll=0;
+      
+      // Board b1 = new Board();   //THIS IS THE PROBLEM
+      
+        while(play){
+            int doubles=0;
+            while(turn==1){
+                if(rollpermission){
+                roll= d1.roll()+d2.roll();
+                    System.out.println("you rolled a "+roll);
+                rollpermission=false;
+                if(d1.getFaceValue()==d2.getFaceValue()){
+                    doubles++;
+                    rollpermission=true;
+                    if(doubles==3){
+                        System.out.println("GO TO JAIL!");
+                    }
+                }
+                p1.move(roll);
+              }
+               
+                System.out.println("You are on square "+p1.getPosition());
+                System.out.println(Board.getSpace(p1.getPosition()));
+                
+           }
+        }
+   
+    
+    
     }
     
 }
