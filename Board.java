cdc6cd3d99a4f3df09ba6f5e398146f40f2f71a4
seanmather <smtothemax1@gmail.com>
2017-05-19 09:09:59 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -12,63 +12,65 @@ package monopoly;
 import java.util.*;
 public class Board{
     //public Property(String placename1,String color1, int square1,int numhouses1,int numhotels1,int rentbasic1, int cost1){
-   public static ArrayList <BoardSpace>wholeboard;
    
-   Property MediterraneanAve = new Property("Boardwalk", "Brown", 2, 0, 0, 2, 50);
-   Property BalticAve = new Property("Baltic Ave", "Brown", 4, 0, 0, 4, 50);
-   Property OrientalAve = new Property("Oriental Ave", "Light-Blue", 7,0, 0, 6, 100);
-   Property VermontAve = new Property("Vermont Ave", "Light-Blue", 9,0, 0, 6, 100);
-   Property ConnecticutAve = new Property("Connecticut Ave", "Light-Blue", 10,0, 8, 50, 120);
    
-   Property StCharlesPlace = new Property("St. Charles Place", "Violet", 12,0, 0, 10, 140);
-   Property StatesAve = new Property("States Ave", "Violet", 14, 0, 0, 10, 140);
-   Property VirginiaAve = new Property("Virginia Ave", "Violet", 15,0, 0, 12, 160);
-   Property StJamesPlace = new Property("St. James Place", "Orange", 17,0, 0, 14, 180);
-   Property TennesseeAve = new Property("Tennessee Ave", "Orange", 19,0, 0, 14, 180);
-   Property NewYorkAve = new Property("New York Ave", "Orange", 20,0, 0, 16, 200);
+   static Property MediterraneanAve = new Property("Boardwalk", "Brown", 2, 0, 0, 2, 50);
+   static Property BalticAve = new Property("Baltic Ave", "Brown", 4, 0, 0, 4, 50);
+  static  Property OrientalAve = new Property("Oriental Ave", "Light-Blue", 7,0, 0, 6, 100);
+   static Property VermontAve = new Property("Vermont Ave", "Light-Blue", 9,0, 0, 6, 100);
+  static  Property ConnecticutAve = new Property("Connecticut Ave", "Light-Blue", 10,0, 8, 50, 120);
    
-   Property KentuckyAve = new Property("Kentucky Ave", "Red", 22,0, 0, 18, 220);
-   Property IndianaAve = new Property("Indiana Ave", "Red", 24,0, 0, 18, 220);
-   Property IllinoisAve = new Property("Illinois Ave", "Red", 25,0, 0, 20, 240);
-   Property AtlanticAve = new Property("Atlantic Ave", "Yellow", 27,0, 0, 22, 260);
-   Property VentnorAve = new Property("Ventnor Ave", "Yellow", 28,0, 0, 22, 260);
-   Property MarvinGardens = new Property("Marvin Gardens", "Yellow", 30,0, 0, 24, 280);
+  static  Property StCharlesPlace = new Property("St. Charles Place", "Violet", 12,0, 0, 10, 140);
+  static  Property StatesAve = new Property("States Ave", "Violet", 14, 0, 0, 10, 140);
+  static  Property VirginiaAve = new Property("Virginia Ave", "Violet", 15,0, 0, 12, 160);
+  static  Property StJamesPlace = new Property("St. James Place", "Orange", 17,0, 0, 14, 180);
+   static Property TennesseeAve = new Property("Tennessee Ave", "Orange", 19,0, 0, 14, 180);
+  static  Property NewYorkAve = new Property("New York Ave", "Orange", 20,0, 0, 16, 200);
    
-   Property PacificAvenue = new Property("Pacific Avenue", "Dark-Green", 32,0, 0, 26, 300);
-   Property NorthCarolinaAve = new Property("North Carolina Ave", "Dark-Green", 33, 0, 0, 26, 300);
-   Property PennsylvaniaAve = new Property("Pennsylvania Ave", "Dark-Green", 35,0, 0, 28, 320);
-   Property ParkPlace = new Property("Park Place", "Dark-Blue", 38,0, 0, 35, 350);
-   Property boardwalk = new Property("Boardwalk", "Dark-Blue", 40, 0, 0, 50, 400);
+ static   Property KentuckyAve = new Property("Kentucky Ave", "Red", 22,0, 0, 18, 220);
+  static  Property IndianaAve = new Property("Indiana Ave", "Red", 24,0, 0, 18, 220);
+  static  Property IllinoisAve = new Property("Illinois Ave", "Red", 25,0, 0, 20, 240);
+  static  Property AtlanticAve = new Property("Atlantic Ave", "Yellow", 27,0, 0, 22, 260);
+  static  Property VentnorAve = new Property("Ventnor Ave", "Yellow", 28,0, 0, 22, 260);
+  static  Property MarvinGardens = new Property("Marvin Gardens", "Yellow", 30,0, 0, 24, 280);
+   
+   static Property PacificAvenue = new Property("Pacific Avenue", "Dark-Green", 32,0, 0, 26, 300);
+   static Property NorthCarolinaAve = new Property("North Carolina Ave", "Dark-Green", 33, 0, 0, 26, 300);
+  static  Property PennsylvaniaAve = new Property("Pennsylvania Ave", "Dark-Green", 35,0, 0, 28, 320);
+  static  Property ParkPlace = new Property("Park Place", "Dark-Blue", 38,0, 0, 35, 350);
+  static  Property boardwalk = new Property("Boardwalk", "Dark-Blue", 40, 0, 0, 50, 400);
     //public Property(String placename1,String color1, int square1,int row1,int numhouses1,int numhotels1,int rentbasic1, int price){
 
    
-   CommunityChest chest1 = new CommunityChest();
-   CommunityChest chest2 = new CommunityChest();
-   CommunityChest chest3 = new CommunityChest();
+   static CommunityChest chest1 = new CommunityChest();
+  static  CommunityChest chest2 = new CommunityChest();
+  static  CommunityChest chest3 = new CommunityChest();
    
-   Chance chance1 = new Chance();
-   Chance chance2 = new Chance();
-   Chance chance3 = new Chance();
+   static Chance chance1 = new Chance();
+  static  Chance chance2 = new Chance();
+  static  Chance chance3 = new Chance();
    
-   Utilities electric = new Utilities();
-   Utilities water = new Utilities();
+  static  Utilities electric = new Utilities();
+  static  Utilities water = new Utilities();
    
-   Fines incometax = new Fines(200);
-   Fines luxurytax = new Fines(100);
-   Fines GO = new Fines(-200);
+  static  Fines incometax = new Fines(200);
+ static   Fines luxurytax = new Fines(100);
+  static  Fines GO = new Fines(-200);
    
-   Railroads BoRail = new Railroads("B & O Railroad");
-   Railroads Reading = new Railroads("Reading Railroad");
-   Railroads Pennsylvania = new Railroads("Pennsylvania Railroad");
-   Railroads Short = new Railroads("Short Line Railroad");
-   //ArrayList<String> x = new ArrayList<String>();
-   //List<String> x = new ArrayList<>(Arrays.asList("xyz", "abc"));
-public Board(){
-ArrayList<BoardSpace> wholeboard1 = new ArrayList<BoardSpace>(Arrays.asList(GO, MediterraneanAve, chest1, BalticAve, incometax, BoRail, OrientalAve, chance1, VermontAve, ConnecticutAve,null, StCharlesPlace, electric, StatesAve, 
+  static  Railroads BoRail = new Railroads("B & O Railroad");
+  static  Railroads Reading = new Railroads("Reading Railroad");
+  static  Railroads Pennsylvania = new Railroads("Pennsylvania Railroad");
+ static   Railroads Short = new Railroads("Short Line Railroad");
+   
+   public static ArrayList<BoardSpace> wholeboard = new ArrayList<BoardSpace>(Arrays.asList(GO, MediterraneanAve, chest1, BalticAve, incometax, BoRail, OrientalAve, chance1, VermontAve, ConnecticutAve,StCharlesPlace, StCharlesPlace, electric, StatesAve, 
 VirginiaAve, Reading, StJamesPlace, chest2, TennesseeAve, NewYorkAve,VentnorAve, KentuckyAve, chance2, IndianaAve, IllinoisAve, Pennsylvania, AtlanticAve, VentnorAve,
 water, MarvinGardens,VentnorAve, PacificAvenue, NorthCarolinaAve, chest3, PennsylvaniaAve, Short, chance3, ParkPlace, luxurytax, boardwalk));
 
-wholeboard=wholeboard1;
+
+   //ArrayList<String> x = new ArrayList<String>();
+   //List<String> x = new ArrayList<>(Arrays.asList("xyz", "abc"));
+public Board(){
+
    // BoardSpace[] row2 = {null, StCharlesPlace, electric, StatesAve, VirginiaAve, Reading, StJamesPlace, chest2, TennesseeAve, NewYorkAve};
    // BoardSpace[] row3 = {null, KentuckyAve, chance2, IndianaAve, IllinoisAve, Pennsylvania, AtlanticAve, VentnorAve, water, MarvinGardens};
     //BoardSpace[] row4 = {null, PacificAvenue, NorthCarolinaAve, chest3, PennsylvaniaAve, Short, chance3, ParkPlace, luxurytax, boardwalk};
@@ -83,6 +85,11 @@ public ArrayList getBoard(){
 public static String getSpace(int place){
 return wholeboard.get(place).toString();
 }
+
+public static BoardSpace whatSpace(int place){
+    return wholeboard.get(place);
+}
+
 public static String toString(int spot){
 
 return "You have landed on"+ Board.getSpace(spot).toString();
@@ -90,6 +97,5 @@ return "You have landed on"+ Board.getSpace(spot).toString();
 
 
 }
-
 
 
