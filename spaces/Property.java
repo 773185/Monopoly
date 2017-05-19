62dc9e71e04129235cd935f394c3a90acc81094e
seanmather <smtothemax1@gmail.com>
2017-05-19 09:11:24 -0500

    Update Property.java

--- spaces/Property.java
+++ spaces/Property.java
@@ -10,26 +10,40 @@ package monopoly;
  * @author 761843
  */
 
-public class Property {
+public class Property extends BoardSpace{
     
     private String placename;
     private String color;
     private int square;
-    private int row;
+    
     private int numhouses;
     private int numhotels;
     private int rentbasic;
     private int price;
+    private boolean owned;
     
-    public Property(String placename1,String color1, int square1,int row1,int numhouses1,int numhotels1,int rentbasic1, int cost1){
-        super(false);
+    public Property(String placename1,String color1, int square1,int numhouses1,int numhotels1,int rentbasic1, int cost1){
+        //super(false);
         placename=placename1;
         color=color1;
         square=square1;
-        row=row1;
+        
         numhouses=numhouses1;
         numhotels=numhotels1;
         rentbasic=rentbasic1;
         price= cost1;
+        owned=false;
     }
+    public boolean isowned (){
+        return owned;
 }
+public String toString(){
+    if(!owned){
+    return "You have landed on "+placename+" which is available for purchase for "+price+" Swanny Dollars. "+super.toString();
+}else{
+        return "You have landed on "+placename+" which is currently owned, you pay rent of "+rentbasic+" Swanny Dollars. "+super.toString();
+    }
+    
+}
+}
+
