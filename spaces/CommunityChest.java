75d1f391e8e73984b1fd21086414900f691ac7c5
seanmather <smtothemax1@gmail.com>
2017-05-23 09:08:29 -0500

    Update CommunityChest.java

--- spaces/CommunityChest.java
+++ spaces/CommunityChest.java
@@ -10,12 +10,22 @@ package monopoly;
  * @author 761843
  */
 public class CommunityChest extends carddraw{
-   private String[] card;
+   private static String[] card;
     public CommunityChest(){
-    String[] cards= new String[3];    
-    cards[0]="Get Money";
-    cards[1]="Get Taxed";
-    cards[2]="Get Reimbursed";
+    String[] cards= new String[4];    
+    cards[0]="Advance to Go";
+    cards[1]="Doctor's fees – Pay $50";
+    cards[2]="Bank error in your favor – Collect $200";
+    cards[3]="Pay school fees of $150";
     card=cards;
     }
+    public static String drawcommunitycard(){
+        int c= (int)(Math.random()*4);
+        return card[c];
+    }
 }
+
+//Advance to Go
+//Bank error in your favor – Collect $200
+//Doctor's fees – Pay $50
+//Pay school fees of $150
