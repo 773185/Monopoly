a4a43fb5992d86fff50e67a62765cd860de19a81
rchen17 <ryan.c761631@gmail.com>
2017-05-27 22:18:29 -0500

    Update Utilities.java

--- spaces/Utilities.java
+++ spaces/Utilities.java
@@ -11,19 +11,49 @@ package monopoly;
  */
 public class Utilities extends BoardSpace{
     private int purchasePrice,baseRent, total;
+    private boolean owned;
+    private String owner;
+    
     public Utilities(String n) {
         super(n);
+        owner = "";
         purchasePrice=150;
+        owned=false;
     }
+    
+    public boolean isowned (){
+        return owned;
+    }
+
+    public void setTrue(){
+    owned=true;
+    }
+
+    public void switchOwner(String n){
+    owner=n;
+    }
+    
+    public int getPrice(){
+        return purchasePrice;
+    }
+    
+    public String getOwner() {
+        return owner;
+    }
+    
     //returns the total rent paid depending on dice roll & utilities owned
-    public void generateRent(int diceRoll, int utilOwned){
+    public int getRent(int diceRoll, int utilOwned){
         total=0;
         if(utilOwned==1)
-            total=4*diceRoll;
+            return total=4*diceRoll;
         
         else
-            total=10*diceRoll;
+           return total=10*diceRoll;
     }
+    
+   // public int getRent(){
+   //     return total;
+    //}
     
     public String toString(){
         return super.toString()+"rent is $"+total;
